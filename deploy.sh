b2c1771d2d8212c7513ba2e8ce5a4eb8bb023f1a
geekhch <709531006@qq.com>
2018-02-02 17:50:22 +0800

    脚本自动提交--geek

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 
 #鸿
-#自动部署博客并将项目同步到github->hexo
+#自动预览、部署博客、并将项目同步到github->hexo
 #
 echo -e "\n***********自动生成静态页面***********\n"
 hexo g
@@ -12,7 +12,7 @@ proxychains4 hexo d
 echo -e  "\n***********部署完成，开始同步项目到github***********\n"
 git add ./
 echo -e "\n***********添加文件完成***********\n"
-git commit -a -m "脚本自动提交"
+git commit -a -m "脚本自动提交--$USER"
 echo -e "\n***********自动commit完成，正在push***********\n"
 proxychains4 git push
 echo -e "\n***********OK！***********\n"
