8ba6bc207858754f6b5a6dd3e915571716af60d4
geekhch <709531006@qq.com>
2019-01-07 11:53:40 +0800

    脚本自动提交--hch

--- hexo-deploy.sh
+++ hexo-deploy.sh
@@ -6,16 +6,16 @@
 
 # exec ssh-agent bash
 # ssh-add ~/ubuntu.rsa
-# echo -e "\n***********自动生成静态页面***********\n"
+echo -e "\n***********自动生成静态页面***********\n"
 hexo g
-# echo -e "\n***********启动本地服务器预览***********\n"
-# hexo s
-# echo -e  "\n***********准备部署到github***********\n"
+echo -e "\n***********启动本地服务器预览***********\n"
+hexo s
+echo -e  "\n***********准备部署到github***********\n"
 hexo d
-# echo -e  "\n***********部署完成，开始同步项目到github***********\n"
+echo -e  "\n***********部署完成，开始同步项目到github***********\n"
 git add .
-# echo -e "\n***********添加文件完成***********\n"
+echo -e "\n***********添加文件完成***********\n"
 git commit -a -m "脚本自动提交--$USER"
-# echo -e "\n***********自动commit完成，正在push***********\n"
+echo -e "\n***********自动commit完成，正在push***********\n"
 git push #git@github.com:geekhch/hexo.git
-# echo -e "\n***********OK！***********\n"
+echo -e "\n***********OK！***********\n"
